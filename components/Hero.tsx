2bb87435e1e765509488345fa229eaeccee69263
bombies <xjamesdzn@gmail.com>
2022-06-09 19:28:50 -0500

    Update hero buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,7 @@ interface Props {
 
 export default function Hero(props: Props) {
     let buttonKey: number = 0, imgButtonKey: number = 0;
-    const buttonsObj = props.buttons ? props.buttons.map(button => !button.isNextButton ? <a key={buttonKey++} className='px-6 py-3 bg-green-600 rounded-lg uppercase text-white font-med text-3xl phone:text-xl drop-shadow-lg transition-all ease-in-out duration-500 hover:scale-105 hover:bg-green-500 hover:drop-shadow-xl' href={button.href}>{button.name}</a> : <Link  href={button.href}><span className='px-6 py-3 bg-green-600 rounded-lg uppercase text-white font-med text-3xl phone:text-xl drop-shadow-lg transition-all ease-in-out duration-500 cursor-pointer hover:scale-105 hover:bg-green-500 hover:drop-shadow-xl'>{button.name}</span></Link>) : '';
+    const buttonsObj = props.buttons ? props.buttons.map(button => !button.isNextButton ? <a key={buttonKey++} className='px-6 py-3 bg-lime-500 rounded-lg uppercase text-white font-med text-3xl phone:text-xl drop-shadow-lg transition-all ease-in-out duration-500 hover:scale-105 hover:bg-green-400 hover:drop-shadow-xl' href={button.href}>{button.name}</a> : <Link  href={button.href}><span className='px-6 py-3 bg-lime-500 rounded-lg uppercase text-white font-med text-3xl phone:text-xl drop-shadow-lg transition-all ease-in-out duration-500 cursor-pointer hover:scale-105 hover:bg-green-400 hover:drop-shadow-xl'>{button.name}</span></Link>) : '';
     const imageButtonsObj = props.imageButtons ? props.imageButtons.map(button => <a key={imgButtonKey++} className='hero--img-button' href={button.href} target={'_blank'} rel={'noreferrer'}><img className='hero--img-button-img' src={button.src} alt={button.alt} />{button.name}</a>) : ''
 
     return (
