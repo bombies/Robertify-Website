0d5aa2326163314f710c47e00bd96c905938a799
bombies <xjamesdzn@gmail.com>
2023-02-27 21:46:40 -0500

    Add redirects

--- next.config.js
+++ next.config.js
@@ -16,6 +16,30 @@ const nextConfig = {
         port: ''
       }
     ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/invite',
+        destination: 'https://discord.com/oauth2/authorize?client_id=893558050504466482&permissions=269479308656&scope=bot%20applications.commands',
+        permanent: false
+      },
+      {
+        source: '/support',
+        destination: 'https://discord.gg/98dD6NbfDU',
+        permanent: false
+      },
+      {
+        source: '/topgg',
+        destination: 'https://top.gg/bot/893558050504466482/vote',
+        permanent: false
+      },
+      {
+        source: '/dbl',
+        destination: 'https://discordbotlist.com/bots/robertify/upvote',
+        permanent: false
+      }
+    ]
   }
 }
 
