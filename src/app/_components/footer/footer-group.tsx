8715150307006294bc47cfca0afc52bea8a8fa00
bombies <xjamesdzn@gmail.com>
2023-03-01 14:40:15 -0500

    Update font size for FooterGroup title

--- src/app/_components/footer/footer-group.tsx
+++ src/app/_components/footer/footer-group.tsx
@@ -7,7 +7,7 @@ interface Props extends React.PropsWithChildren {
 export default function FooterGroup(props: Props) {
     return (
         <div className='flex flex-col gap-4'>
-            <h3 className='font-semibold !text-primary dark:drop-shadow-glow-primary-lg'>{props.heading}</h3>
+            <h3 className='font-semibold !text-primary text-lg dark:drop-shadow-glow-primary-lg'>{props.heading}</h3>
             <div className='phone:text-sm flex flex-col gap-4'>
                 {props.children}
             </div>
