eac350fd4068c239eb7f3a003643718fc88af86d
bombies <xjamesdzn@gmail.com>
2023-03-09 13:33:34 -0500

    Fix a bug where content in heading sections couldn't be interacted with

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout(props: Props) {
         <Providers>
             <ProgressBar />
             <div className='dark:visible opacity-50 blur-xl invisible fixed w-full h-full z-[0]'>
-                <Image className='blur-xl' src={glow} alt='' fill={true} />
+                <Image draggable={false} className='blur-xl z-[0]' src={glow} alt='' fill={true} />
             </div>
             {/* @ts-expect-error Async Server Component */}
             <NavbarContainer/>
