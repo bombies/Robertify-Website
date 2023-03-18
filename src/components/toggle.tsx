d636a5a26dd2739fb93274ab9ed1b7d0f9126015
bombies <xjamesdzn@gmail.com>
2023-03-18 12:15:37 -0500

    Fix bug where toggles wouldn't handle click event

--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -10,10 +10,11 @@ export default function Toggle(props: Props) {
     return (
         <div
             className={'relative w-12 h-6 transition-fast rounded-full dark:bg-dark bg-neutral-100 shadow-md cursor-pointer ' + (props.status ? '!bg-primary' : '') + (props.disabled ? ' brightness-50' : '')}
-            onClick={() => {
+            onClick={(e) => {
                 if (props.disabled)
                     return;
-                return props.onClick;
+                if (props.onClick)
+                    props.onClick(e);
             }}
         >
             <div className={'absolute transition-fast h-6 w-6 rounded-full bg-primary shadow-md dark:shadow-primary/30 ' + (props.status ? 'right-0 !bg-white' : 'left-0')}></div>
