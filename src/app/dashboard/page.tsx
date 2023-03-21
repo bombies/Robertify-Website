0a4a1563094e5e5ea524b6b3922ee1286d4dc6c6
bombies <xjamesdzn@gmail.com>
2023-03-21 10:36:18 -0500

    Fix error handling when routing to dashboard while not logged in

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,17 +2,19 @@ import GuildGrid from "@/app/dashboard/guild-grid";
 import WebClient from "@/utils/api/web-client";
 import {cookies} from "next/headers";
 import {AxiosError} from "axios";
-import Card from "@/components/card";
 
 const getUserGuilds = async (token?: string) => {
     try {
         return (await (await WebClient.getInstance())
             .get(`/api/discord/users/${token}/guilds`))?.data;
     } catch (e: any) {
-        if (e instanceof AxiosError && e.response?.data.retry_after) {
-            setTimeout(() => {
-                getUserGuilds(token);
-            }, e.response.data.retry_after)
+        if (e instanceof AxiosError) {
+            if (e.response?.data.retry_after) {
+                setTimeout(() => {
+                    getUserGuilds(token);
+                }, e.response.data.retry_after)
+            } else if (e.response?.status === 404)
+                return Promise.resolve(undefined);
         } else throw e;
     }
 }
@@ -20,6 +22,7 @@ const getUserGuilds = async (token?: string) => {
 export default async function Dashboard() {
     const token = cookies().get('login-token')?.value;
     const guilds = (await getUserGuilds(token))?.data;
+
     return (
         <main className='p-12 phone:px-6 min-h-screen'>
             <h1 className='text-primary text-5xl phone:text-3xl dark:drop-shadow-glow-primary-lg text-center mb-6'>
