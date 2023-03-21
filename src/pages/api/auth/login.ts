15bdabcc61428af276c9622ecce72d75bd971ea1
bombies <xjamesdzn@gmail.com>
2023-03-21 11:14:16 -0500

    Implement stronger password verification

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,7 +1,7 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {HTTPMethod, MethodHandler} from "@/utils/api/method-handler";
 import {ReasonPhrases, StatusCodes} from "http-status-codes";
-import {sign} from "jsonwebtoken";
+import {sign, verify} from "jsonwebtoken";
 import {nanoid} from "nanoid";
 
 class RouteHandler extends MethodHandler {
@@ -18,13 +18,19 @@ class RouteHandler extends MethodHandler {
                 if (!password)
                     return this.prepareResponse(401, "You must provide a password field in the body!");
 
-                if (password !== process.env.API_MASTER_PASSWORD)
+                if (!this.verifyPassword(password))
                     return utils.prepareUnauthorizedResponse();
 
                 const token = sign({id: nanoid(6)}, process.env.API_SECRET_KEY!, {expiresIn: '5m', subject: 'admin'});
                 return this.prepareResponse(StatusCodes.OK, ReasonPhrases.OK, {access_token: token});
             })
             .execute();
+    }
+
+    private verifyPassword(password: string) {
+        if (!password)
+            return false;
+        return !!verify(password, process.env.API_SECRET_KEY!);
     }
 
 }
