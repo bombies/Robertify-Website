f60a82e58215ecb5c4bf40cca1648148a2156141
bombies <xjamesdzn@gmail.com>
2023-03-23 09:20:37 -0500

    Fix footer display on phones

--- src/app/_components/footer/footer.tsx
+++ src/app/_components/footer/footer.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 export default function Footer() {
     return (
-        <div className='bg-neutral-200 dark:bg-dark/50 p-24 tablet:p-16 flex justify-between gap-4'>
+        <div className='bg-neutral-200 dark:bg-dark/50 p-24 tablet:p-16 phone:p-3 flex phone:flex-col justify-between gap-4'>
             <div>
-                <div className='relative w-16 h-16 mb-6'>
+                <div className='relative w-16 h-16 mb-6 phone:mb-2'>
                     <Image
                         draggable={false}
                         src='https://i.imgur.com/fwG8qA5.png'
@@ -17,9 +17,9 @@ export default function Footer() {
                         sizes='4rem'
                     />
                 </div>
-                <p className='text-neutral-400 dark:text-neutral-500 max-w-xs font-light text-sm'>A discord music bot that with a multitude of features that will fit your liking.</p>
+                <p className='text-neutral-400 dark:text-neutral-500 max-w-xs phone:max-w-3/4 font-light text-sm phone:text-[.75rem]'>A discord music bot that with a multitude of features that will fit your liking.</p>
             </div>
-            <div className='flex gap-16 phone:gap-4'>
+            <div className='flex gap-16 phone:gap-4 phone:justify-center'>
                 <FooterGroup heading='Robertify'>
                     <HyperLink href={process.env.NEXT_PUBLIC_DISCORD_INVITE_LINK || ''}>Invite</HyperLink>
                     <HyperLink href={'/support'}>Support Server</HyperLink>
