9bf5aa7e851ac9eef2a9a9f8a94350144d93fa7d
bombies <xjamesdzn@gmail.com>
2023-03-28 23:45:15 -0500

    Start doc implementation

---
+++ theme.config.jsx
@@ -0,0 +1,18 @@
+export default {
+    useNextSeoProps() {
+      return {
+          titleTemplate: 'Robertify - %s'
+      }
+    },
+    logo: <span>Robertify Docs</span>,
+    project: {
+        link: 'https://github.com/bombies/Robertify-Bot',
+    },
+    darkMode: true,
+    footer: {
+        text: <span>
+      MIT {new Date().getFullYear()} © <a href="https://robertify.me" target="_blank">Robertify</a>.
+    </span>
+    },
+    faviconGlyph: "📖"
+}
