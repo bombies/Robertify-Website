e716429a7f3910e9ecf63960241cadab0c6bf944
bombies <xjamesdzn@gmail.com>
2023-03-29 12:08:37 -0500

    Continue docs

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,18 +1,26 @@
+import GenericImage from "./src/app/_components/GenericImage";
+
 export default {
     useNextSeoProps() {
-      return {
-          titleTemplate: 'Robertify - %s'
-      }
+        return {
+            titleTemplate: 'Robertify - %s'
+        }
     },
-    logo: <span>Robertify Docs</span>,
+    logo: (
+        <>
+            <span>Robertify Docs</span>
+        </>
+        ),
     project: {
         link: 'https://github.com/bombies/Robertify-Bot',
     },
     darkMode: true,
+    primaryHue: 136,
     footer: {
         text: <span>
       MIT {new Date().getFullYear()} © <a href="https://robertify.me" target="_blank">Robertify</a>.
     </span>
-    },
+    }
+    ,
     faviconGlyph: "📖"
 }
