332e43c0115481a1cf0c613311ab8afacbb2d134
bombies <xjamesdzn@gmail.com>
2023-05-03 23:56:20 -0500

    Update not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,14 @@
+import Card from "@/components/card";
+import Link from "next/link";
+
 export default function NotFound() {
     return (
-        <>
-            <h1>Not found</h1>
-        </>
+        <main className='h-screen flex justify-center items-center'>
+            <div className='text-center'>
+                <h1 className='text-9xl font-bold text-primary tracking-widest drop-shadow-glow-primary-lg'>404</h1>
+                <h3>I couldn't find what you were looking for. :(</h3>
+                <p className='trakcing-wide'>Need help? Check out our <Link href='/docs'>documentation</Link></p>
+            </div>
+        </main>
     )
 }
